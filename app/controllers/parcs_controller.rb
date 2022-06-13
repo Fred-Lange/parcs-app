2137ef1941d1af6db3265bf1146e981c173c79e8
Fred Lange <pro.frederic.lange@gmail.com>
2022-06-13 17:33:13 +0200

    ajout de l'index et de la show

--- app/controllers/parcs_controller.rb
+++ app/controllers/parcs_controller.rb
@@ -1,2 +1,30 @@
 class ParcsController < ApplicationController
+  def index
+    @parcs = Parc.all
+  end
+
+  # def show
+  #   @parcs = Parc.find(params[:id])
+  # end
+
+  # def new
+  #   @parc = Parc.new # needed to instantiate the form_for
+  # end
+
+  # def create
+  #   @parc = Parc.new(parc_params)
+  #   if @parc.save
+  #     redirect_to parc_path(@parc)
+  #   else
+  #     render "new"
+  # end
+
+  # def update
+  # end
+
+  # private
+
+  # def parc_params
+  #   params.require(:parc).permit(:name, :age, :equipment, :note, :water, :wc, :address)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "parcs#index"
+  resources :parcs, only: [ :index, :show, :new, :create, :update ] do
+    resources :posts, only: [ :new, :create ]
+  end
 end
