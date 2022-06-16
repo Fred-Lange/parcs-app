0d9c03d4b99f928a7bc2e9edbcf9e6806047f9b3
Fred Lange <pro.frederic.lange@gmail.com>
2022-06-16 12:00:26 +0200

    param de la view d'un commentaire et maj du controller sur les commentaires

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,24 @@
 class PostController < ApplicationController
   def new
+    @parc = Parc.find(params[:parc_id])
+    @post = Post.new
   end
 
   def create
+    @parc = Parc.find(params[:parc_id])
+    @post = Post.new(post_params)
+    @post.parc = @parc
+    if @post.save
+      redirect_to parc_path(@parc)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:rating, :content)
   end
 end
