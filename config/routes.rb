99e551c5bd31469796d9c63c56665ef194b3ff97
Fred Lange <pro.frederic.lange@gmail.com>
2022-06-16 11:39:16 +0200

    ajout controller et vue post avec action new et create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'post/new'
+  get 'post/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "parcs#index"
   resources :parcs, only: [ :index, :show, :new, :create, :update ] do
